f23a444c18ceb04dce617fda523130f152a03225
Brandon Kowalski <brandonkowalski@kwall.org>
2013-07-09 23:11:19 -0400

    Setup gh-pages.

--- javascript/build.js
+++
@@ -1,2 +0,0 @@
-//@codekit-prepend "angular.js", "angular-resource.js", "angular-sanitize.js", "app.js", "controllers.js";
-console.log('powered by μProse.\nhttps://github.com/BrandonKowalski/uProse');
